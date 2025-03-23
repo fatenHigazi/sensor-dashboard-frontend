dc3a14300272034037ff9a6750d178ec1fd3e230
fatenHigazi <fatenhigazi@gmail.com>
2025-03-23 07:32:04 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
-// app.config.ts (Angular 17+ example)
+
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(), // <-- add this
+    provideHttpClient(), 
   ]
 };
